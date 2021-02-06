393a5f39fb3cac926c7252451ec0f62f6e966d56
wflixu <wflixu@yeah.net>
2021-02-06 18:50:47 +0800

    add ignores

--- index.js
+++ index.js
@@ -2,27 +2,24 @@ const fs = require('fs');
 const path = require('path');
 
 
-module.exports = function (target) {
+module.exports = function (target, ignores) {
     let base = target || './';
     let basePath = path.resolve(base);
     let output = {};
-    readDir(basePath, output);
-    console.log(output);
+    readDir(basePath, output,ignores); //
     let targetPath = path.resolve('./dirText.text');
     generateText(output, targetPath);
 
 }
 
-function readDir(targetPath, output) {
-    console.log('targetPath', targetPath);
-    console.log('output', output);
+function readDir(targetPath, output,blackList) {
+   
     let arr = fs.readdirSync(targetPath);
     arr.forEach(item => {
-        console.log(item)
         const curPath = path.resolve(targetPath, item);
         let isDirectory = fs.statSync(curPath).isDirectory();
         if (isDirectory) {
-            if ((/^\.\w+/ig).test(item)) {
+            if (isIgnore(blackList, item)) {
                 // do nothing
             } else {
                 output[item] = {};
@@ -35,10 +32,14 @@ function readDir(targetPath, output) {
     })
 }
 
+function isIgnore(blackList, item) {
+    return ((/^\.\w+/ig).test(item) || (Array.isArray(blackList) && blackList.includes(item)));
+}
+
 function generateText(data, targetPath) {
     //   let str = JSON.stringify(data,null,2);
     let str = '';
-    str +=  dir(data,1)
+    str += dir(data, 1)
     fs.writeFileSync(targetPath, str);
 }
 
@@ -51,7 +52,7 @@ function dir(data, level) {
         let isLast = index === arr.length - 1;
         str = str + row(level, isLast, item);
         if (typeof data[item] !== 'string') {
-            str += dir(data[item],level+1);
+            str += dir(data[item], level + 1);
         }
     })
 
